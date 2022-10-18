8f2a0aad2bc80ea2617aa7dc50b8ccbbd61c9c2d
unknown <venceslavam@gmail.com>
2022-10-18 16:42:34 +0300

    first commit

---
+++ WebMenus/Pages/Restaurants.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMenus.Pages
+{
+    public class RestaurantsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
