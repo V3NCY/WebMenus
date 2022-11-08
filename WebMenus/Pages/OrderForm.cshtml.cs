65598955bcaa6b5e3dd62448893ffd74ce26fd63
unknown <venceslavam@gmail.com>
2022-11-08 14:12:14 +0200

    Added OrderForm

---
+++ WebMenus/Pages/OrderForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMenus.Pages
+{
+    public class OrderFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
